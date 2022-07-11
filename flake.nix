a40709665f2f15949ef03b50d6883ff062a17626
Joshua Gilman <joshuagilman@gmail.com>
2022-07-11 14:16:49 -0700

    build: adds codebuild spec file (#1)

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/21.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in
+      {
         devShell = pkgs.mkShell {
           buildInputs = [
-            pkgs.git
-            pkgs.nodejs-17_x
+            pkgs.nodejs-16_x
             pkgs.nodePackages.gatsby-cli
           ];
         };
